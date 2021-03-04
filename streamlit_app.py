2755114cc711a9bb3121e9f3f2b88adbb9b91df6
henosBot <spi0003@boxhillhs.vic.edu.au>
2021-03-04 13:23:08 +1100

    keys

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,20 +24,20 @@ while not loggedin:
     count += 1
     user_id = id.text_input('User ID', key=count)
     user_pass = passw.text_input('Password', type='password', key=count+1)
-    login = button.button('Login')
+    login = button.button('Login', key=count+2)
     if login:
         with st.spinner('Logging in ...'):
             time.sleep(0.5)
             tf, reason = tools.auth(user_id, user_pass)
         if tf is False and reason == 'id':
             incor = st.error('I could not find a user with that id')
-            try_again = button.button('Try again')
+            try_again = button.button('Try again', key=count+3)
             if try_again:
                 incor.empty()
                 continue
         elif tf is False and reason == 'pass':
             incor = st.error('Incorrect Password')
-            try_again = button.button('Try again')
+            try_again = button.button('Try again', key=count+4)
             if try_again:
                 incor.empty()
                 continue
@@ -57,7 +57,7 @@ markdown.markdown(tools.viewAfterLogin)
 
 mark = st.empty()
 ctrl = st.empty()
-markb = mark.button('Mark Roll')
-ctrlb = ctrl.button('Control Panel')
+markb = mark.button('Mark Roll', key=count+5)
+ctrlb = ctrl.button('Control Panel', key=count+6)
 if markb:
     tools.mark_roll(user_id)
