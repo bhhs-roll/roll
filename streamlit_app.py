47837f468ee8e33b6d194b2675e7b1a2cedcb8df
henosBot <spi0003@boxhillhs.vic.edu.au>
2021-03-09 10:51:09 +1100

    again

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ import time
 import face_recognition
 
 st.title('henos Roll Taking system')
-dir(face_recognition)
+st.text(dir(face_recognition))
 markdown = st.empty()
 markdown.markdown(tools.viewBeforeLogin)
 id = st.empty()
